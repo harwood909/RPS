921ee1b4a38ff1e8aa291d45a67845d5c082da8e
Shane <harwood909@gmail.com>
2022-03-19 20:19:56 -0600

    Prompt for userChoice is persistant until a valid option is given ie. rock paper or scissors

--- source.js
+++ source.js
@@ -3,22 +3,23 @@ function computerPlay(){
     let randomNumber = Math.round(Math.random()*(100))
     switch(true){
         case (randomNumber <= 33):
-        console.log("Rock");
+        console.log("rock");
         console.log(randomNumber);
-        return "Rock";
+        return "rock";
         break;
 
         case (randomNumber > 33 && randomNumber <= 66):
-        console.log("Paper");
+        console.log("paper");
         console.log(randomNumber);
-        return "Paper";
+        return "paper";
         break;
 
         case (randomNumber > 66):
-        console.log("Scissors");
+        console.log("scissors");
         console.log(randomNumber);
-        return "Scissors";
+        return "scissors";
         break;
+
         default:
         alert(`The computer picked ${randomNumber}, no decision was made`);
 
@@ -26,6 +27,29 @@ function computerPlay(){
 }
 userPlay()
 function userPlay(){
-    let userChoice = prompt("Pick your weapon", "Rock");
-    console.log(userChoice);
+    while (typeof(userChoice) != "string"){
+        let userChoice = prompt("Pick your weapon: Rock, Paper or Scissors", "Rock");
+        if (userChoice != null){
+            userChoice = userChoice.toLowerCase();
+        }
+        switch (true){
+            case (userChoice == "rock"):
+        console.log(userChoice);
+        return userChoice;
+        break;
+
+        case (userChoice == "paper"):
+            console.log(userChoice);
+            return userChoice;
+            break;
+            
+            case (userChoice == "scissors"):
+                console.log(userChoice);
+        return userChoice;
+        break;
+        
+        default:
+            console.log("Good job, you broke it")
+        }
+    }
 }
