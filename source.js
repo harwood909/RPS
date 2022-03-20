3eb65bbbf5584af3a199cc9811bf2498fbb2be17
Shane <harwood909@gmail.com>
2022-03-19 18:26:19 -0600

    Add prompt for userChoice input and display to console

--- source.js
+++ source.js
@@ -1,25 +1,31 @@
-/* console.log(randomNumber) */
-
 computerPlay()
 function computerPlay(){
-    let randomNumber = Math.random()*(100)
+    let randomNumber = Math.round(Math.random()*(100))
     switch(true){
         case (randomNumber <= 33):
         console.log("Rock");
         console.log(randomNumber);
+        return "Rock";
         break;
 
         case (randomNumber > 33 && randomNumber <= 66):
         console.log("Paper");
         console.log(randomNumber);
+        return "Paper";
         break;
 
         case (randomNumber > 66):
         console.log("Scissors");
         console.log(randomNumber);
+        return "Scissors";
         break;
         default:
         alert(`The computer picked ${randomNumber}, no decision was made`);
 
     }
+}
+userPlay()
+function userPlay(){
+    let userChoice = prompt("Pick your weapon", "Rock");
+    console.log(userChoice);
 }
